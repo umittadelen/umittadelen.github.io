3ea6928ac2249e41c0582a26b649700ace9990e6
umittadelen <umittadelen1277@gmail.com>
2026-05-07 21:28:20 +0200

    Update builder.js

--- scripts/builder.js
+++ scripts/builder.js
@@ -2,6 +2,7 @@ const sections = [
     {
         header: "PROJECTS:",
         elements: [
+            { text: "Wireframe Cube Renderer", link: "https://www.umittadelen.net/wireframeRender/" },
             { text: "3D Maze Generator", link: "https://www.umittadelen.net/3DMazeGen/" },
             { text: "Triangle Calculator", link: "https://www.umittadelen.net/tricalc/" },
             { text: "Tetris", link: "https://www.umittadelen.net/TETRIS/" },
